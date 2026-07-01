c2a44f7b204f96cf13a920f7f172d0cc03beb7f3
nishant-singh21 <nishantsingh1721@gmail.com>
2026-07-01 18:18:24 +0530

    generic crud

--- apiProject03/api/urls.py
+++ apiProject03/api/urls.py
@@ -1,7 +1,17 @@
+# from django.urls import path
+# from .views import StudentAPI
+
+# urlpatterns = [
+#     path('students/',StudentAPI.as_view()),# for get (all) and Post 
+#     path('students/<int:pk>/',StudentAPI.as_view()),# for GET (single) , PUY, DELETE
+
+# ]
+
+
 from django.urls import path
-from .views import StudentAPI
+from .views import StudentListCreateAPI, StudentRetrieveUpdateDeleteAPI
 
 urlpatterns = [
-    path('students/',StudentAPI.as_view()),# for get (all) and Post 
-    path('students/<int:pk>/',StudentAPI.as_view()),# for GET (single) , PUY, DELETE
+    path('students/', StudentListCreateAPI.as_view()),
+    path('students/<int:pk>/', StudentRetrieveUpdateDeleteAPI.as_view()),
 ]
--- apiProject03/api/views.py
+++ apiProject03/api/views.py
@@ -1,59 +1,110 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .models import Student
-from .serializers import  StudentSerializer
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+# from .models import Student
+# from .serializers import  StudentSerializer
+
+from rest_framework import generics , mixins
+from .models import Student 
+from .serializers import StudentSerializer
 
 
-# CRUD oprations using APIView
+# CRUD oprb using GenericAPIView and Mixins
+class StudentListCreateAPI(
+    generics.GenericAPIView, 
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin
+):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
-class StudentAPI(APIView):
-    # Read ALL or singhle data 
-      def get(self, request, pk=None):
-            if pk:
-                try:
-                     student = Student.objects.get(id = pk)
-                     serializer = StudentSerializer(student)
-                     return Response(serializer.data, status = status.HTTP_200_OK)
-                except Student.DoesNotExist:
-                    return Response({'detail': 'Student does not exist'}, status = status.HTTP_404_NOTFOUND)
+    #read all data
+    def get(self, request,*args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-            else:
-                #  Read all data 
-                students = Student.objects.all()
-                serializer = StudentSerializer(students , many = True)
-                return Response(serializer.data, status = status.HTTP_200_OK)
+    # create data
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+
+class StudentRetrieveUpdateDeleteAPI(
+    generics.GenericAPIView,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin
+):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    # Get  single data
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    #  PUT update data
+    def post(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    # delete data
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+
+# # CRUD oprations using APIView
+
+# class StudentAPI(APIView):
+#     # Read ALL or singhle data 
+#       def get(self, request, pk=None):
+#             if pk:
+#                 try:
+#                      student = Student.objects.get(id = pk)
+#                      serializer = StudentSerializer(student)
+#                      return Response(serializer.data, status = status.HTTP_200_OK)
+#                 except Student.DoesNotExist:
+#                     return Response({'detail': 'Student does not exist'}, status = status.HTTP_404_NOTFOUND)
+
+#             else:
+#                 #  Read all data 
+#                 students = Student.objects.all()
+#                 serializer = StudentSerializer(students , many = True)
+#                 return Response(serializer.data, status = status.HTTP_200_OK)
             
-    # create data(POST)
-      def post(self, request):
-           serializer = StudentSerializer(data=request.data)
-           if serializer.is_valid():
-               serializer.save()
-               return Response(serializer.data, status = status.HTTP_201_CREATED)
-           return Response(serializer.errors, status = status.HTTP_400_BADREQUEST)
+#     # create data(POST)
+#       def post(self, request):
+#            serializer = StudentSerializer(data=request.data)
+#            if serializer.is_valid():
+#                serializer.save()
+#                return Response(serializer.data, status = status.HTTP_201_CREATED)
+#            return Response(serializer.errors, status = status.HTTP_400_BADREQUEST)
 
-    # update data(PUT)
-      def put(self, request , pk):
-           try:
-               student = Student.objects.all(pk=pk)
-           except  Student.DoesNotExist:
-                return Response({ "error": "Student not found "},  status =status.HTTP_404  )
+#     # update data(PUT)
+#       def put(self, request , pk):
+#            try:
+#                student = Student.objects.all(pk=pk)
+#            except  Student.DoesNotExist:
+#                 return Response({ "error": "Student not found "},  status =status.HTTP_404  )
            
-           serializer = StudentSerializer(student, partial = True )
+#            serializer = StudentSerializer(student, partial = True )
             
-           if serializer.is_valid():
-               serializer.save()
-               return Response(serializer.data , status = status.HTTP_200_OK)
-           return Response(serializer.error, status=status.HTTP_404_BAD_NOTFOUNDREQUESt)
+#            if serializer.is_valid():
+#                serializer.save()
+#                return Response(serializer.data , status = status.HTTP_200_OK)
+#            return Response(serializer.error, status=status.HTTP_404_BAD_NOTFOUNDREQUESt)
 
-    #   delete data(DELETE)
-      def delete(self , request ,pk):
-          try:
-                student = Student.objects.get(id = pk)
-                student.delete()
-          except Student.DoesNotExist:
-               return Response({"error": "Student does not exist"}, status = status.HTTP_404_NOTFOUND)
-  
+#     #   delete data(DELETE)
+#       def delete(self , request ,pk):
+#           try:
+#                 student = Student.objects.get(id = pk)
+#                 student.delete()
+#           except Student.DoesNotExist:
+#                return Response({"error": "Student does not exist"}, status = status.HTTP_404_NOTFOUND)
+
+
+
+
+
+
          
       
            
